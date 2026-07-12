e36b450f3a5ae617ad2ebb22e964080d623bc04b
alitradersmkn <zamanmaqsood453@gmail.com>
2026-07-12 12:59:15 +0500

    Implement image slideshow in script.js
    Added image slideshow functionality that changes the displayed image every 3 seconds.

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ const footer = document.querySelector("footer p");
 if (footer) {
     footer.innerHTML = "&copy; " + new Date().getFullYear() + " Ali Traders & Commission Shop. All Rights Reserved.";
 }
+const images=[
+"images/shop.jpg",
+"images/shop2.jpg",
+"images/shop3.jpg"
+];
+
+let i=0;
+
+setInterval(function(){
+
+i++;
+
+if(i>=images.length){
+
+i=0;
+
+}
+
+document.getElementById("slide").src=images[i];
+
+},3000);
